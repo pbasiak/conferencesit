d2e54102fe09bced7b1c8f85acee963038b3a48f
pawel.basiak01@gmail.com <pawel.basiak01@gmail.com>
2019-06-23 11:17:04 +0200

    Add example data

--- src/conferences.js
+++ src/conferences.js
@@ -1,4 +1,23 @@
 export const conferencesList = [
-    { "name": "Name" },
-    { "name": "Name 2" }
+    {
+        "name": "CodeMash",
+        "location": "Sandusky, Ohio",
+        "date": "08.01.2019",
+        "submission_deadline": "31.08.2018",
+        "url": "http://www.codemash.org/",
+    },
+    {
+        "name": "JSFoo",
+        "location": "Pune, India",
+        "date": "10.01.2019",
+        "submission_deadline": "31.09.2018",
+        "url": "https://jsfoo.in/2019-pune/",
+    },
+    {
+        "name": "Forward JS",
+        "location": "San Francisco, California",
+        "date": "24.01.2019",
+        "submission_deadline": "01.08.2018",
+        "url": "https://forwardjs.com/",
+    },
 ];
